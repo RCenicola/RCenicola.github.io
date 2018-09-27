046bb5477e93cabfd5a5d98ccaa6053f63b5bd41
RCenicola <rcenicola11@gmail.com>
2018-09-27 16:10:17 -0700

    change bio

--- js/main.js
+++ js/main.js
@@ -355,19 +355,3 @@
 	window.GridFx = GridFx;
 
 })(window);
-
-// Leverage event-delegation via bubbling
-document.addEventListener( "click", function toggleWords ( event ) {
-    // A few variables to help us track important values/references
-    var target = event.target, values = [], placed;
-    // If the clicked element has multiple values
-    if ( target.hasAttribute( "data-values" ) ) {
-        // Split those values out into an array
-        values = target.getAttribute( "data-values" ).split( "," );
-        // Find the location of its current value in the array
-        // IE9+ (Older versions supported by polyfill: http://goo.gl/uZslmo)
-        placed = values.indexOf( target.textContent );
-        // Set its text to be the next value in the array
-        target.textContent = values[ ++placed % values.length ];
-    }
-});
